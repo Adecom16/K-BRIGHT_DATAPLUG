fafde4f5c4f705a6a819920ed74d02a62b77138a
unknown <adefokunadeoluwaisrael@gmail.com>
2024-01-11 13:08:13 +0100

    fix useAuth

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -139,12 +139,12 @@ export const AuthProvider = ({ children }) => {
 // AuthContext.js
 // ... (your existing code)
 
-export const useAuth = () => {
-     const context = useContext(AuthContext);
+// export const useAuth = () => {
+//      const context = useContext(AuthContext);
 
-     if (!context) {
-          throw new Error('useAuth must be used within an AuthProvider');
-     }
+//      if (!context) {
+//           throw new Error('useAuth must be used within an AuthProvider');
+//      }
 
-     return context;
-};
+//      return context;
+// };
